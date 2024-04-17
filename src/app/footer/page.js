6f770e2f020d28ac4038832f12fdb88ccc444cd6
Sainath Kamble <kamblesainath41@gmail.com>
2024-04-17 15:55:14 +0530

    chinese gp upate

--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -7,7 +7,7 @@ export default function Footer(){
     return(
         <section className="h-[12vh] w-screen bg-black grid grid-cols-1 grid-rows-2 gap-2 place-items-center py-2">
            <p className="text-slate-50 text-md font-medium sm:text-xl md:text-xl lg:xl xl:twxt-xl 2xl:text-2xl ">Created and maintained by Sainath Kamble</p>
-           <div className="h-auto w-1/4 flex justify-evenly items-cente">
+           <div className="h-auto w-2/5 flex justify-evenly items-cente">
            <Link href="https://github.com/sainathkamble">
              <FontAwesomeIcon icon={faGithub} className="h-5 w-5 text-slate-50 sm:text-xl md:text-xl lg:xl xl:twxt-xl 2xl:h-6 2xl:w-6"/>
            </Link>
--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -8,7 +8,7 @@ export default function Navbar(){
         <nav className="h-[8vh] w-screen bg-black text-slate-50 text-xl flex justify-evenly items-end">
           <Link href="./" className="h-full w-1/3 flex justify-center items-center text-lg font-semibold hover:bg-slate-800 lg:text-lg xl:text-xl 2xl:text-xl">Home</Link>
           <Link href="./livestream" className="h-full w-1/3 flex justify-center items-center text-lg font-semibold  hover:bg-slate-800 lg:text-lg xl:text-xl 2xl:text-xl">Livestrem</Link>
-          <Link href="./racetime" className="h-full w-1/3 flex justify-center items-center text-lg font-semibold  hover:bg-slate-800 lg:text-lg xl:text-xl 2xl:text-xl">Race time</Link>
+          {/* <Link href="./racetime" className="h-full w-1/3 flex justify-center items-center text-lg font-semibold  hover:bg-slate-800 lg:text-lg xl:text-xl 2xl:text-xl">Race time</Link> */}
           <Link href="./about" className="h-full w-1/3 flex justify-center items-center text-lg font-semibold  hover:bg-slate-800 lg:text-lg xl:text-xl 2xl:text-xl">About</Link>
         </nav>
         </>
