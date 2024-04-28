7e5797e94ca3e9ae49e2272ecd43d6a5925199e5
Sainath Kamble <kamblesainath41@gmail.com>
2024-04-28 19:00:50 +0530

    changed background imagr

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import "./global.css";
 import Navbar from "./navbar/page";
 import Footer from "./footer/page";
 import Hero from "./home/page";
-import Image from "next/image";
 
 
 export default function Home() {
@@ -10,8 +9,6 @@ export default function Home() {
   return (
     <div className="h-screen w-screen overflow-x-hidden">
       <Navbar/>
-      {/* <Image className="h-[82vh] w-full bg-cover -z-10"
-        src="/herobg.jpg" width={100} height={100} alt="Background image"></Image> */}
       <Hero />
       <Footer/>
     </div>
