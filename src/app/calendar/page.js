1e56458c8edcab48e8ad021a1368d98e2858ac38
Sainath Kamble <kamblesainath41@gmail.com>
2024-04-30 19:24:24 +0530

    Added Time zone checking feature

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -9,7 +9,7 @@ export default function Calender(){
         <div className="bg-image h-[90vh] w-screen grid place-items-center bg-cover text-slate-50 bg-slate-950 overflow-x-hidden overflow-y-hidden">
          <Navbar/>
          <div className="h-[80vh] w-screen flex justify-center items-center">
-           <Image src="/calendar.webp" width="400" height="900" className="bg-cover" alt="F1 2024 Calendar image"/>
+           <Image src="./calendar.webp" width="400" height="900" className="bg-cover" alt="F1 2024 Calendar image"/>
          </div>
         </div>
         <Footer/>
