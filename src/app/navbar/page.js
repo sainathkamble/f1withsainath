64d6597d6e227a978fb99d87ae5394833af165fa
Sainath Kamble <kamblesainath41@gmail.com>
2024-04-30 19:22:16 +0530

    Time zon update

--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -5,27 +5,27 @@ export default function Navbar(){
 
     return(
         <>
-        <nav className="h-[10vh] w-screen bg-transparent text-white text-xl flex justify-evenly items-end">
-          <Link href="./" className="nav-item h-full w-1/3 text-lg font-bold lg:text-2lg xl:text-2xl 2xl:text-2xl
-           grid grid-cols-1 grid-rows-1 place-items-center">
+        <nav className="h-[10vh] w-screen bg-transparent text-xl flex justify-evenly items-end">
+          <Link href="./" className="nav-item h-full w-1/3 text-lg font-semibold sm:text-sm sm:font-medium md:text-md lg:text-lg xl:text-2xl 2xl:text-2xl
+           grid grid-cols-1 grid-rows-1 place-items-center text-white hover:text-red-600">
             <p>Home</p>
             <div className="nav-underline h-1 w-full bg-red-600 invisible"></div>
           </Link>
 
-          <Link href="./calendar" className="nav-item h-full w-1/3 text-lg font-bold lg:text-2lg xl:text-2xl 2xl:text-2xl
-          grid grid-cols-1 grid-rows-1 place-items-center">
+          <Link href="./calendar" className="nav-item h-full w-1/3 text-lg font-semibold sm:text-sm sm:font-medium md:text-md lg:text-lg xl:text-2xl 2xl:text-2xl
+          grid grid-cols-1 grid-rows-1 place-items-center text-white hover:text-red-600">
             <p>Calendar</p>
             <div className="nav-underline h-1 w-full bg-red-600 invisible"></div>
           </Link>
 
-          {/* <Link href="./racetime" className="nav-item h-full w-1/3 flex text-lg font-semibold lg:text-lg xl:text-xl 2xl:text-xl
-          grid grid-cols-1 grid-rows-1 place-items-center">
-           <p>Race time</p>
+          <Link href="./racetime" className="nav-item h-full w-1/3 text-lg font-semibold sm:text-sm sm:font-medium md:text-md lg:text-lg xl:text-2xl 2xl:text-2xl
+          grid grid-cols-1 grid-rows-1 place-items-center text-white hover:text-red-600">
+           <p>GP time</p>
            <div className="nav-underline h-1 w-full bg-red-600 invisible"></div>
-          </Link> */}
+          </Link>
 
-          <Link href="./about" className="nav-item h-full w-1/3 text-lg font-bold lg:text-2lg xl:text-2xl 2xl:text-2xl
-          grid grid-cols-1 grid-rows-1 place-items-center">
+          <Link href="./about" className="nav-item h-full w-1/3 text-lg font-semibold sm:text-sm sm:font-medium md:text-md lg:text-lg xl:text-2xl 2xl:text-2xl
+          grid grid-cols-1 grid-rows-1 place-items-center text-white hover:text-red-600">
             <p>About</p>
             <div className="nav-underline h-1 w-full bg-red-600 invisible"></div>
           </Link>
