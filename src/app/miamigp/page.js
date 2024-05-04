8da37c41ef108793b9cef4451ecbebb836e8a6fe
Sainath Kamble <kamblesainath41@gmail.com>
2024-05-04 22:44:37 +0530

    Update

---
+++ src/app/miamigp/page.js
@@ -0,0 +1,17 @@
+import "../global.css";
+import Navbar from "../navbar/page";
+import Footer from "../footer/page";
+
+export default function Home() {
+    return (
+      <>
+      <section className="bg-image h-[90vh] w-screen bg-transparent">
+      <Navbar/>
+      <iframe id="iframe" src="https://latifistreams.com/channel/raw2?url=aHR0cHM6Ly9pbnN0MS5pZ25vcmVzLnRvcC9qcy9mMS1xdWFsaWZ5aW5nLWYxLW1haW4tcmFjZS8xL3BsYXlsaXN0Lm0zdTg=" 
+      allow="autoplay; encrypted-media" height="94%" width="100%" frameborder="0" scrolling="no" allowfullscreen=""></iframe>
+      </section>
+      <Footer/>
+      </>
+    );
+  }
+  
