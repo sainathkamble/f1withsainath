59a38d25a1d534a8558b0d0945abc37acd88ae2d
Sainath Kamble <kamblesainath41@gmail.com>
2024-06-12 21:32:52 +0530

    The image file was missing :)

--- src/app/calendar/page.js
+++ src/app/calendar/page.js
@@ -9,7 +9,7 @@ export default function Calender(){
         <section className="bg-image h-[100vh] w-screen grid place-items-center bg-cover text-slate-50 bg-slate-950 overflow-x-hidden overflow-y-hidden">
          <Navbar/>
          <div className="h-[80vh] w-screen flex justify-center items-center">
-           <Image src="./calendar.webp" width="400" height="900" className="bg-cover" alt="F1 2024 Calendar image"/>
+           <Image src="/calendar.webp" width="400" height="900" className="bg-cover" alt="F1 2024 Calendar image"/>
          </div>
          <Footer/>
         </section>
