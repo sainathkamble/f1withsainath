fd6fe5564eec4cc2b71d24613e8a093716350694
Sainath Kamble <kamblesainath41@gmail.com>
2024-06-13 14:54:31 +0530

    Le mans 2024 Updates

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -7,10 +7,10 @@ import {React , useState} from "react";
 
 export default function Hero(){
 
-  let dayBeforeGp = new Date("June 20, 2024").getDate();
-  let firstGpDate = new Date("June 21, 2024").getDate();
-  let secondGpDate = new Date("June 22, 2024").getDate();
-  let finalGpDate = new Date("June 23, 2024").getDate();
+  let dayBeforeGp = new Date("June 13, 2024").getDate();
+  let firstGpDate = new Date("June 14, 2024").getDate();
+  let secondGpDate = new Date("June 15, 2024").getDate();
+  let finalGpDate = new Date("June 16, 2024").getDate();
     
      //Get race date  logic automatic
      let [gpDayOne , setGpDayOne] = useState(new Date().getDate());
@@ -18,15 +18,15 @@ export default function Hero(){
      let [gpDayThree , setGpDayThree] = useState(new Date().getDate());
 
      gpDayOne == dayBeforeGp || gpDayOne ==  firstGpDate || gpDayOne == secondGpDate || gpDayOne == finalGpDate ?
-     setGpDayOne = new Date("June 21, 2024").getDate() : setGpDayOne = "--";
+     setGpDayOne = new Date("June 13, 2024").getDate() : setGpDayOne = "--";
 
      gpDayTwo == dayBeforeGp || gpDayTwo ==  firstGpDate || gpDayTwo == secondGpDate || gpDayTwo == finalGpDate ?
-      setGpDayTwo = new Date("June 22, 2024").getDate() : setGpDayTwo = "--";
+      setGpDayTwo = new Date("June 14, 2024").getDate() : setGpDayTwo = "--";
 
      gpDayThree == dayBeforeGp || gpDayThree ==  firstGpDate || gpDayThree == secondGpDate || gpDayThree == finalGpDate ?
-     setGpDayThree = new Date("June 23, 2024").getDate() : setGpDayThree = "--";
+     setGpDayThree = new Date("June 15, 2024").getDate() : setGpDayThree = "--";
      
-     let currentGp = ""; let gpHeader = "GP 2024"; let gpButton = "Grand Prix";
+     let currentGp = ""; let gpHeader = "2024"; let gpButton = "2024";
 
      const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
      const date = new Date();
@@ -35,7 +35,7 @@ export default function Hero(){
      let currentDate = new Date().getDate();
 
      if(currentDate == dayBeforeGp || currentDate == firstGpDate || gpDayOne == secondGpDate || currentDate == finalGpDate ){
-      currentGp = "Spanish"; gpHeader = gpHeader; gpButton = gpButton;
+      currentGp = "Le Mans"; gpHeader = gpHeader; gpButton = gpButton;
      }else{
       currentGp = "No GP scheduled for now";
       gpHeader = ""; gpButton = "";
@@ -55,17 +55,18 @@ return(
 
               <div className="h-auto w-auto text-white text-lg">
                 <p className="flex justify-center">{setGpDayOne} {currentMonth}</p>
-                <p>Free Practice 1 :-  11:00 PM</p>
-                <p>Free Practice 2 :- 2:30 AM</p>
+                <p>Free Practice 1 :- 1:30 AM</p>
+                <p>Free Practice 2 :- 6:30 PM</p>
+                <p>Hyperpole :- 11:30 PM</p>
               </div>
               <div className="h-auto w-auto text-white text-lg">
                 <p className="flex justify-center">{setGpDayTwo} {currentMonth}</p> 
-                <p>Free Practice 3 :- 10:00 PM</p>
-                <p>Qualifying :- 1:30 AM</p>
+                <p>Free Practice 3 :- 1:30 AM</p>
               </div>
               <div className="h-auto w-auto text-white text-lg">
                 <p className="flex justify-center">{setGpDayThree} {currentMonth}</p>
-                <p>Race :- 11:30 PM</p>
+                <p>Warm Up :- 3:30 PM</p>
+                <p>Race :- 7:30 PM</p>
               </div>
               <Link href="./spanishgp" className="px-4 py-2 rounded-xl text-lg font-semibold text-white bg-red-600">
                 {currentGp} {gpButton}
